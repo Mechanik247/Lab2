4db117d07b775425596f5cc3136e947b4dd1fcbc
Mechanik247 <37372647+Mechanik247@users.noreply.github.com>
2018-03-25 21:12:13 +0500

    Update Department.java

--- humanResources/Department.java
+++ humanResources/Department.java
@@ -1,27 +1,26 @@
 package humanResources;
 
-import java.io.Serializable;
-
-public class Department implements Serializable {
+public class Department {
     //todo: такая же фигня как и в емплоях
-    public static final int CountOfElementsInArray = 8;
+    public static final int DEFAULT_SIZE_OF_ARRAY = 8;
+    public static final String DEFAULT_NAME = "";
     private String name;
     private Employee[] employees;
     private int size;
 
     //todo: и здесь тоже
     public Department() {
-        this("", CountOfElementsInArray);
+        this(DEFAULT_NAME, DEFAULT_SIZE_OF_ARRAY);
     }
 
     public Department(String name) {
-        this(name, CountOfElementsInArray);
+        this(name, DEFAULT_SIZE_OF_ARRAY);
     }
 
     //todo: к - тоже не лучшее имя для параметра
-    public Department(String name, int k) {
+    public Department(String name, int size) {
         this.name = name;
-        employees = new Employee[k];
+        employees = new Employee[size];
         this.size=0;
     }
 
@@ -38,46 +37,34 @@ public class Department implements Serializable {
         this.name = name;
     }
 
-//todo: refact
+    //todo: refact
     public void add(Employee employee) {
-        int k = employees.length;
-        boolean checkExistNull = false;
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] == null) checkExistNull = true;
+        if(size == employees.length)
+        {
+            Employee[] employeesTemp = new Employee[size * 2];
+            System.arraycopy(employees, 0, employeesTemp, 0, size);
+            employees = employeesTemp;
+            employees[size] = employee;
+            size++;
         }
-        if (checkExistNull) {
-            for (int i = 0; i < employees.length; i++) {
-                if (employees[i] == null) {
-                    employees[i] = employee;
-                    break;
-                }
-            }
-        } else {
-            int n = 0;
-            Employee[] employees2 = new Employee[k * 2];
-            System.arraycopy(employees, 0, employees2, 0, k);
-            employees2[k] = employee;
-            employees = employees2;
+        else {
+            employees[size] = employee;
+            size++;
         }
     }
 
     public boolean remove(String fName, String sName) {
-        int k = 0;
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i].getFirstName() == fName && employees[i].getSecondName() == sName) {
+        int sizeEmployees = employees.length;
+        for (int i = 0; i < sizeEmployees; i++) {
+            if (employees[i] != null && employees[i].getFirstName().equals(fName) && employees[i].getSecondName().equals(sName)) {
                 employees[i] = null;
-                k++;
-                for (int j = i; j < employees.length - 1; j++) {
-                    if (employees[j + 1] != null) {
-                        employees[j] = employees[j + 1];
-                    } else {
-                        employees[j] = null;
-                    }
-                }
+                System.arraycopy(employees, i + 1, employees, i, sizeEmployees - (i + 1));
+                employees[sizeEmployees - 1] = null;
+                size--;
+                return true;
             }
         }
-        if (k == 1) return true;
-        else return false;
+        return false;
     }
 
     public int size() {
@@ -85,33 +72,37 @@ public class Department implements Serializable {
     }
 
     public Employee[] getEmployees() {
-        return employees;
+        Employee[] employeesTemp = new Employee[size];
+        System.arraycopy(employees, 0, employeesTemp, 0, size);
+        return employeesTemp;
     }
 
     public Employee[] getEmployeesByTitle(String jTitle) {
-        int p = employees.length;
-        int r = 0;
-        Employee[] temp = new Employee[p];
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i].getJobTitle() == jTitle) {
-                temp[r] = employees[i];
-                r++;
+        int lengthArray = employees.length;
+        int tempSize = 0;
+        Employee[] temp = new Employee[lengthArray];
+        for (int i = 0; i < lengthArray; i++) {
+            if (employees[i].getJobTitle().equals(jTitle)) {
+                temp[tempSize] = employees[i];
+                tempSize++;
             }
         }
         return temp;
     }
 
-    public Employee[] getSortedEmployeesBySalary(Employee[] employees) {
+    public Employee[] getSortedEmployeesBySalary() {
         Employee temp = new Employee();
-        for (int j = 0; j < employees.length; j++) {
-            for (int k = 0; k < employees.length - 1; k++) {
-                if (employees[k].getSalary() > employees[k + 1].getSalary()) {
-                    temp = employees[k];
-                    employees[k] = employees[k + 1];
-                    employees[k + 1] = temp;
+        Employee[] tempSortedArray = new Employee[size];
+        System.arraycopy(employees, 0, tempSortedArray, 0, size);
+        for (int j = 0; j < tempSortedArray.length; j++) {
+            for (int k = 0; k < tempSortedArray.length - 1; k++) {
+                if (tempSortedArray[k].getSalary() > tempSortedArray[k + 1].getSalary()) {
+                    temp = tempSortedArray[k];
+                    tempSortedArray[k] = tempSortedArray[k + 1];
+                    tempSortedArray[k + 1] = temp;
                 }
             }
         }
-        return employees;
+        return tempSortedArray;
     }
 }
